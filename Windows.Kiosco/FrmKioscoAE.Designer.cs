862f4c94a18d61e6d20242bb1fdc326145ed4d4a
FacundoCandermo <kqcandermo0@gmail.com>
2025-06-22 19:39:34 -0300

    RamaDeWindows

---
+++ Windows.Kiosco/FrmKioscoAE.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Windows.Kiosco
+{
+    partial class FrmKioscoAE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNombre = new Label();
+            txtNombre = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            txtPrecioBase = new TextBox();
+            txtStock = new TextBox();
+            dtpFechaIngreso = new DateTimePicker();
+            dtpFechaVto = new DateTimePicker();
+            cboTipo = new ComboBox();
+            cboMarca = new ComboBox();
+            btnOk = new Button();
+            btnCancelar = new Button();
+            SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(12, 9);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(51, 15);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(125, 9);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 1;
+            txtNombre.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Precio Base";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Stock";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Feha Ingreso";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 147);
+            label4.Name = "label4";
+            label4.Size = new Size(107, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Fecha Vencimiento";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(291, 17);
+            label5.Name = "label5";
+            label5.Size = new Size(83, 15);
+            label5.TabIndex = 6;
+            label5.Text = "Tipo Producto";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(291, 54);
+            label6.Name = "label6";
+            label6.Size = new Size(40, 15);
+            label6.TabIndex = 7;
+            label6.Text = "Marca";
+            // 
+            // txtPrecioBase
+            // 
+            txtPrecioBase.Location = new Point(125, 46);
+            txtPrecioBase.Name = "txtPrecioBase";
+            txtPrecioBase.Size = new Size(100, 23);
+            txtPrecioBase.TabIndex = 8;
+            txtPrecioBase.TextChanged += txtPrecioBase_TextChanged;
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(125, 75);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(100, 23);
+            txtStock.TabIndex = 9;
+            // 
+            // dtpFechaIngreso
+            // 
+            dtpFechaIngreso.Location = new Point(125, 103);
+            dtpFechaIngreso.Name = "dtpFechaIngreso";
+            dtpFechaIngreso.Size = new Size(200, 23);
+            dtpFechaIngreso.TabIndex = 10;
+            // 
+            // dtpFechaVto
+            // 
+            dtpFechaVto.Location = new Point(125, 141);
+            dtpFechaVto.Name = "dtpFechaVto";
+            dtpFechaVto.Size = new Size(200, 23);
+            dtpFechaVto.TabIndex = 11;
+            // 
+            // cboTipo
+            // 
+            cboTipo.FormattingEnabled = true;
+            cboTipo.Location = new Point(380, 17);
+            cboTipo.Name = "cboTipo";
+            cboTipo.Size = new Size(121, 23);
+            cboTipo.TabIndex = 12;
+            // 
+            // cboMarca
+            // 
+            cboMarca.FormattingEnabled = true;
+            cboMarca.Location = new Point(337, 46);
+            cboMarca.Name = "cboMarca";
+            cboMarca.Size = new Size(121, 23);
+            cboMarca.TabIndex = 13;
+            // 
+            // btnOk
+            // 
+            btnOk.Location = new Point(66, 203);
+            btnOk.Name = "btnOk";
+            btnOk.Size = new Size(136, 56);
+            btnOk.TabIndex = 14;
+            btnOk.Text = "Aceptar";
+            btnOk.UseVisualStyleBackColor = true;
+            btnOk.Click += btnOk_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(257, 203);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(136, 56);
+            btnCancelar.TabIndex = 15;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FrmKioscoAE
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnOk);
+            Controls.Add(cboMarca);
+            Controls.Add(cboTipo);
+            Controls.Add(dtpFechaVto);
+            Controls.Add(dtpFechaIngreso);
+            Controls.Add(txtStock);
+            Controls.Add(txtPrecioBase);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Name = "FrmKioscoAE";
+            Text = "FrmKioscoAE";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox txtPrecioBase;
+        private TextBox txtStock;
+        private DateTimePicker dtpFechaIngreso;
+        private DateTimePicker dtpFechaVto;
+        private ComboBox cboTipo;
+        private ComboBox cboMarca;
+        private Button btnOk;
+        private Button btnCancelar;
+    }
+}
---
+++ Windows.Kiosco/FrmKioscoAE.cs
@@ -0,0 +1,249 @@
+﻿using Kisoco.Datos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows.Kiosco
+{
+    public partial class FrmKioscoAE : Form
+    {
+        private Producto? producto = null!;
+
+        public FrmKioscoAE()
+        {
+            InitializeComponent();
+            this.Load += FrmKioscoAE_Load;
+            cboTipo.SelectedIndexChanged += cboTipo_SelectedIndexChanged;
+
+        }
+        private void cboTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboMarca.Items.Clear();
+
+            switch (cboTipo.SelectedItem.ToString())
+            {
+                case "Golosina":
+                    foreach (var marca in Enum.GetValues(typeof(MarcaG)))
+                    {
+                        cboMarca.Items.Add(marca);
+                    }
+                    break;
+                case "Cigarrillo":
+                    foreach (var marca in Enum.GetValues(typeof(MarcaC)))
+                    {
+                        cboMarca.Items.Add(marca);
+                    }
+                    break;
+                case "Bebida":
+                    foreach (var marca in Enum.GetValues(typeof(MarcaB)))
+                    {
+                        cboMarca.Items.Add(marca);
+                    }
+                    break;
+            }
+
+            if (cboMarca.Items.Count > 0)
+                cboMarca.SelectedIndex = 0;
+        }
+
+
+        public FrmKioscoAE()
+        {
+            InitializeComponent();
+
+            CargarComboTipo();
+
+            cboTipo.SelectedIndexChanged += cboTipo_SelectedIndexChanged;
+        }
+
+
+        private void FrmKioscoAE_Load(object sender, EventArgs e)
+        {
+            CargarCombos();
+        }
+
+        private void CargarCombos()
+        {
+            cboTipo.DataSource = Enum.GetValues(typeof(TipoGolosina));
+            cboMarca.DataSource = Enum.GetValues(typeof(MarcaG));
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrecioBase_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!ValidarDatos())
+            {
+                return;
+            }
+
+            string nombre = txtNombre.Text;
+            double precioBase = double.Parse(txtPrecioBase.Text);
+            int stock = int.Parse(txtStock.Text);
+            DateTime fechaIngreso = dtpFechaIngreso.Value.Date;
+            DateTime fechaVto = dtpFechaVto.Value.Date;
+
+            string tipoSeleccionado = cboTipo.SelectedItem.ToString()!;
+
+            if (tipoSeleccionado == "Golosina")
+            {
+                var tipoG = (TipoGolosina)cboTipo.SelectedItem!;
+                var marcaG = (MarcaG)cboMarca.SelectedItem!;
+                producto = new Golosina(tipoG, marcaG)
+                {
+                    Nombre = nombre,
+                    PrecioBase = precioBase,
+                    stock = stock,
+                    fechaIngreso = fechaIngreso,
+                    fechaVto = fechaVto
+                };
+            }
+            else if (tipoSeleccionado == "Cigarrillo")
+            {
+                var marcaC = (MarcaC)cboMarca.SelectedItem!;
+                producto = new Cigarrillo(marcaC, true) 
+                {
+                    Nombre = nombre,
+                    PrecioBase = precioBase,
+                    stock = stock,
+                    fechaIngreso = fechaIngreso,
+                    fechaVto = fechaVto
+                };
+            }
+            else if (tipoSeleccionado == "Bebida")
+            {
+                var marcaB = (MarcaB)cboMarca.SelectedItem!;
+                producto = new Bebida(marcaB, 1, false) 
+                {
+                    Nombre = nombre,
+                    PrecioBase = precioBase,
+                    stock = stock,
+                    fechaIngreso = fechaIngreso,
+                    fechaVto = fechaVto
+                };
+            }
+
+            DialogResult = DialogResult.OK;
+        }
+
+        public Producto? GetProducto()
+        {
+            return producto;
+        }
+
+
+        private bool ValidarDatos()
+        {
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("Debe ingresar un nombre.");
+                return false;
+            }
+
+            if (!double.TryParse(txtPrecioBase.Text, out _))
+            {
+                MessageBox.Show("Precio base inválido.");
+                return false;
+            }
+
+            if (!int.TryParse(txtStock.Text, out _))
+            {
+                MessageBox.Show("Stock inválido.");
+                return false;
+            }
+
+            if (cboTipo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar un tipo de producto.");
+                return false;
+            }
+
+            if (cboMarca.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar una marca.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private Producto? CrearProductoDesdeFormulario()
+        {
+            string nombre = txtNombre.Text.Trim();
+
+            if (!double.TryParse(txtPrecioBase.Text, out double precioBase))
+                throw new Exception("Precio base inválido.");
+
+            if (!int.TryParse(txtStock.Text, out int stock))
+                throw new Exception("Stock inválido.");
+
+            DateTime fechaIngreso = dtpFechaIngreso.Value.Date;
+            DateTime fechaVto = dtpFechaVto.Value.Date;
+
+            string tipoSeleccionado = cboTipo.SelectedItem.ToString();
+            object marcaSeleccionada = cboMarca.SelectedItem;
+
+            if (string.IsNullOrEmpty(nombre))
+                throw new Exception("El nombre es requerido.");
+
+            if (marcaSeleccionada == null)
+                throw new Exception("Debe seleccionar una marca.");
+
+            switch (tipoSeleccionado)
+            {
+                case "Golosina":
+                    MarcaG marcaG = (MarcaG)marcaSeleccionada;
+                    return new Golosina((TipoGolosina)0, marcaG) 
+                    {
+                        Nombre = nombre,
+                        PrecioBase = precioBase,
+                        stock = stock,
+                        fechaIngreso = fechaIngreso,
+                        fechaVto = fechaVto
+                    };
+
+                case "Cigarrillo":
+                    MarcaC marcaC = (MarcaC)marcaSeleccionada;
+                    return new Cigarrillo(marcaC, false) 
+                    {
+                        Nombre = nombre,
+                        PrecioBase = precioBase,
+                        stock = stock,
+                        fechaIngreso = fechaIngreso,
+                        fechaVto = fechaVto
+                    };
+
+                case "Bebida":
+                    MarcaB marcaB = (MarcaB)marcaSeleccionada;
+                    return new Bebida(marcaB, 1, false) 
+                    {
+                        Nombre = nombre,
+                        PrecioBase = precioBase,
+                        stock = stock,
+                        fechaIngreso = fechaIngreso,
+                        fechaVto = fechaVto
+                    };
+
+                default:
+                    throw new Exception("Tipo de producto inválido");
+            }
+        }
+
+
+    }
+}
